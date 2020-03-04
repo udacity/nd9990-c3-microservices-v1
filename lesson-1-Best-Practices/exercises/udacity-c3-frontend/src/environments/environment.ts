4775b882e1c3f74ef4af94c238b980ddfd3241f7
Charles Doyle <lguniversity@ymail.com>
2020-03-03 20:28:09 -0800

    deleted everything to have a local git repo for each exercise

--- lesson-1-Best-Practices/exercises/udacity-c3-frontend/src/environments/environment.ts
+++ lesson-1-Best-Practices/exercises/udacity-c3-frontend/src/environments/environment.ts
@@ -8,7 +8,8 @@ export const environment = {
   apiHost: 'http://localhost:8080/api/v0'
 };
 
-/*
+/* apiHost: http://localhost:8080/api/v0
+   apiHost: 
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
