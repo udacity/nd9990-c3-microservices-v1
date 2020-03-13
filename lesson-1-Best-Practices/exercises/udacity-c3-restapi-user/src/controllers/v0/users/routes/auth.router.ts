95e33bb083460190015ec68aed67f6311130c0cc
Charles Doyle <lguniversity@ymail.com>
2020-03-12 19:32:07 -0700

    change files to follow lessons

--- lesson-1-Best-Practices/exercises/udacity-c3-restapi-user/src/controllers/v0/users/routes/auth.router.ts
+++ lesson-1-Best-Practices/exercises/udacity-c3-restapi-user/src/controllers/v0/users/routes/auth.router.ts
@@ -28,25 +28,25 @@ function generateJWT(user: User): string {
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
- //  return next();
-    if (!req.headers || !req.headers.authorization){
-        return res.status(401).send({ message: 'No authorization headers.' });
-    }
-    
-
-    const token_bearer = req.headers.authorization.split(' ');
-    if(token_bearer.length != 2){
-        return res.status(401).send({ message: 'Malformed token.' });
-    }
-    
-    const token = token_bearer[1];
-    return jwt.verify(token, c.config.jwt.secret , (err, decoded) => {
-      if (err) {
-        return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
-      }
-      return next();
-    });
-}
+    //  return next();
+       if (!req.headers || !req.headers.authorization){
+           return res.status(401).send({ message: 'No authorization headers.' });
+       }
+       
+   
+       const token_bearer = req.headers.authorization.split(' ');
+       if(token_bearer.length != 2){
+           return res.status(401).send({ message: 'Malformed token.' });
+       }
+       
+       const token = token_bearer[1];
+       return jwt.verify(token, c.config.jwt.secret , (err, decoded) => {
+         if (err) {
+           return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+         }
+         return next();
+       });
+   }
 
 router.get('/verification', 
     requireAuth, 
